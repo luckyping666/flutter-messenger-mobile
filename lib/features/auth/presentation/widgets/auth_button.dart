9680e842386da57e51275e311f3295f8516785e6
Roman <kuklin.prog@gmail.com>
2025-09-23 12:12:44 +0800

    add auth widget: button

---
+++ lib/features/auth/presentation/widgets/auth_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:messanger/core/theme.dart';
+
+class AuthButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  
+  const AuthButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: DefaultColors.buttonColor,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+        padding: EdgeInsets.symmetric(vertical: 15),
+      ),
+      child: Text(text, style: TextStyle(color: Colors.white)),
+    );
+  }
+}
